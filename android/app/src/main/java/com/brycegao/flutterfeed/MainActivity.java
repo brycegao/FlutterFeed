eb1f31e202e13f01f9d2835231c9d4dcefc7627f
gaorui006 <gaorui006@ke.com>
2019-03-14 20:36:32 +0800

    test

---
+++ android/app/src/main/java/com/brycegao/flutterfeed/MainActivity.java
@@ -0,0 +1,13 @@
+package com.brycegao.flutterfeed;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
